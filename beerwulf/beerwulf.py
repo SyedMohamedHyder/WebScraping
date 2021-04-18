b51c5bfe27b0c8ee7a176b9c2027809b9ca0bda2
SyedMohamedHyder <syedhyder1362k@gmail.com>
2021-04-18 11:40:10 +0530

    Completed beerwulf

---
+++ beerwulf/beerwulf.py
@@ -0,0 +1,47 @@
+from requests_html import HTMLSession
+import pandas as pd
+
+url = 'https://www.beerwulf.com/en-gb/c/subkegs?container=SUB%20Kegs%20-%202L%20keg&catalogCode=Beer_1'
+beerlist = []
+
+def render_page(url):
+    print('Rendering Page.')
+    s = HTMLSession()
+    r = s.get(url)
+    r.html.render(sleep=1) # sleep=1 is important - sleeps for x seconds after the render
+    products = r.html.xpath('//*[@id="product-items-container"]/a')
+    return products
+
+def parse(products):
+    print('Getting beerwulf info..')
+    for item in products:
+        name  = item.find('h4', first=True).text
+        beer_type = item.find('p', first=True).text
+        price = item.find('span', first=True).text
+        links = item.absolute_links
+
+        if item.search('Temporarily sold out'):
+            stock = 'out of stock'
+        else:
+            stock = 'in stock'
+
+        product_info = {
+        'Name': name,
+        'Price': price,
+        'Beer Type': beer_type,
+        'Stock': stock,
+        'URL': links
+        }
+        beerlist.append(product_info)
+
+        print(f'Found {len(beerlist)} item(s)')
+
+def output():
+    df = pd.DataFrame(beerlist)
+    print('Saved to .xls')
+    df.to_csv('beer-list.csv', index=False)
+
+
+html = render_page(url)
+parse(html)
+output()
